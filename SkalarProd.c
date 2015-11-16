224f1362ebaab9888cbb0c388edcc395b078e1a3
narv1 <narv@onders.org>
2015-11-16 21:23:09 +0100

    adding few lines

---
+++ SkalarProd.c
@@ -0,0 +1,51 @@
+/*******************************************************************************************/
+/*                           Willkommen zum ersten Projekt!                                "/
+/*               Dieses Programm zielt darauf ab einen besseren Umgang mit:                */
+/*                          Übungen mit Vektoren und Matrizen                              */
+/*                                     -Arrays                                             */
+/*                    -Verbesserung der Eingabe mit Stringzerlegung                        */
+/*                         -Verwendung von Zufallszahlen                                   */
+/*                   -Berechnung einfacher mathematischer Sachverhalt                      */
+/*******************************************************************************************/
+
+
+/*******************************************************************************************/
+/*                               Author: Stefan Hermeter                                   */
+/*******************************************************************************************/
+
+
+/*****************************************/
+/*      Die verwendeten Libaries         */
+/* bzw. die maximale Anzahl der Vektoren */
+/*****************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#define N = 3
+
+/**************************************/
+/*           Nebenprogramm            */
+/*     multiplizieren des Arrays      */
+/**************************************/
+
+int skalar_produkt(int A, int B){
+  int ergebnis;
+
+  
+  return ergebnis;
+}
+
+/***************************************/
+/*     Main ist das Hauptprogramm      */
+/***************************************/
+int main (){
+
+  int *p, skalar1[N], skalar[N];
+  int i;
+  
+  srand =
+  
+  skalar_produkt();
+
+  
+  return 0;
+}
