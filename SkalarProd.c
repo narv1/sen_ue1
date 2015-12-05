9b511516649909927d6143bc8c8776abc7e2b693
narv1 <narv@onders.org>
2015-12-05 02:50:13 +0100

    bericht added teil 1 schema

--- SkalarProd.c
+++ SkalarProd.c
@@ -51,14 +51,6 @@ int matrizen_addition(int A, int B){
   return ergebnis;
 }
 
-int matrix_multiplikation(int A, int B){
-  int ergebnis;
-
-  ergebnis = A * B;
-
-  return ergebnis;
-}
-
 /***************************************/
 /*     Main ist das Hauptprogramm      */
 /***************************************/
@@ -74,6 +66,8 @@ int main (){
   // Kommentar des Programms
   printf("Dieses Programm ist in 4 Teil Programme augespalten:\nMit den Arrays wird ein Skalarprodukt erzeugt, eine Matrix addition und eine Matrix multiplikation,\n");
 
+  printf("\n_____________________________________\n");
+
   // Zufallszahlen generieren und speichern | Ausgabe
   printf("Die Einzelwerte des Arrays1 sind:\n");
   for ( i = 0; i < N; i++){
@@ -81,6 +75,7 @@ int main (){
     printf("skalar1[%d]\t%d\n", i, skalar1[i]);
   }
 
+  printf("\n_____________________________________\n");
 
   // Zufallszahlen generieren und speichern | Ausgabe
   printf("Die Einzelwerte des Arrays2 sind:\n");
@@ -88,6 +83,8 @@ int main (){
     skalar2[i] = ( rand() % 100 + 1);
     printf("skalar2[%d]\t%d\n", i, skalar2[i]);
   }          
+
+  printf("\n_____________________________________\n");
   
   /**************************************/
   /*  pointer übergabe von einlesewert  */
@@ -104,9 +101,11 @@ int main (){
   // Ausgabe der Multiplikation SKALARPRODUKT
   printf("Das Skalarprodukt lautet:\n");
   for ( i = 0; i < N; i++){
-    printf("Proudukt[%d]:%d\n", i, end[i]);
+    printf("Proudukt[%d]:\t%d\n", i, end[i]);
   }
 
+  printf("\n_____________________________________\n");
+  
   /**************************************/
   /*  pointer übergabe von einlesewert  */
   /*    Aufruf der Matrizenrechnung     */
@@ -122,14 +121,16 @@ int main (){
   // Ausgabe der Addition MATRIZENADDITION
   printf("Das Matrizen addition lautet:\n");
   for ( i = 0; i < N; i++){
-    printf("Proudukt[%d]:%d\n", i, end[i]);
+    printf("Proudukt[%d]:\t%d\n", i, end[i]);
   }
 
+  printf("\n_____________________________________\n");
+  
   int matpr1[N][N], matpr2[N][N], mat_erg[N][N];
   int j;
 
-  // automatisches befüllen des Arrays
-  printf("Die Zahlen werden automatisch befüllt:\n");
+  // Automatisches befüllen des Arrays
+  printf("_____________________________________\nDie Zahlen werden automatisch befüllt:\n");
   for ( i = 0; i < N; i++){
     for ( j = 0; j < N; j++){
       matpr1[i][j] = (rand() % 100+1);
@@ -138,12 +139,12 @@ int main (){
 
   for (i = 0; i < N; i++){
     for (j = 0; j < N; j++){
-      printf("Matrizen[%d][%d] \t%d\n", i, j, matpr1[i][j]);
+      printf("Matrix1[%d][%d] \t%d\n", i, j, matpr1[i][j]);
     }
   }
 
-  // automatisches befüllen des Arrays
-  printf("Die Zahlen werden automatisch befüllt:\n");
+  // Automatisches befüllen des Arrays
+  printf("_____________________________________\nDie Zahlen werden automatisch befüllt:\n");
   for ( i = 0; i < N; i++){
     for ( j = 0; j < N; j++){
       matpr2[i][j] = (rand() % 100+1);
@@ -152,9 +153,11 @@ int main (){
 
   for (i = 0; i < N; i++){
     for (j = 0; j < N; j++){
-      printf("Matrizen[%d][%d] \t%d\n", i, j, matpr2[i][j]);
+      printf("Matrix2[%d][%d] \t%d\n", i, j, matpr2[i][j]);
     }
   }
+
+  printf("\n_____________________________________\n");
   
   int k;
 
